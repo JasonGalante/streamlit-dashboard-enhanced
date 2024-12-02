ec0ca036bf0aa6ccbafc7126075c231412954928
Jason Galante <jasonmgalante@gmail.com>
2024-12-02 14:06:19 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,5 +9,5 @@ st.text("""And everyone clapped.""")
 
 
 # Display a GIF
-gif_path = "C:\\Users\\grift\\OneDrive\\Documents\\GitHub\\streamlit-dashboard-enhanced\\clap gif.webp" 
+st.image("https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHU0bzFlMjNkeDFjNWFpZnlldHEzYnNyejZtdnQ3ODlhOG5xaWdrNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/zGnnFpOB1OjMQ/giphy.gif", caption="Online GIF")
 st.image(gif_path, caption="Your GIF", use_container_width=True)
