72c3650d0c3cf18f6cdaba201334a5119b2c2ad3
Jason Galante <jasonmgalante@gmail.com>
2024-12-02 14:02:31 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,52 +1,13 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
+import io
+st.header("Jason Galante 2024 AHI 507 Streamlit Example")
+st.subheader("Proof of concept")
 
-st.header("2024 AHI 507 Streamlit Example")
-st.subheader("We are going to go through a couple different examples of loading and visualization information into this dashboard")
-
-st.text("""In this streamlit dashboard, we are going to focus on some recently released school learning modalities data from the NCES, for the years of 2021.""")
-
-# ## https://healthdata.gov/National/School-Learning-Modalities-2020-2021/a8v3-a3m3/about_data
-df = pd.read_csv("https://healthdata.gov/resource/a8v3-a3m3.csv?$limit=50000") ## first 1k 
-
-## data cleaning 
-df['week_recoded'] = pd.to_datetime(df['week'])
-df['zip_code'] = df['zip_code'].astype(str)
-
-df['week'].value_counts()
-
-## box to show how many rows and columns of data we have: 
-col1, col2, col3 = st.columns(3)
-col1.metric("Columns", df.shape[1]) 
-col2.metric("Rows", len(df))
-col3.metric("Number of unique districts/schools:", df['district_name'].nunique())
-
-## exposing first 1k of NCES 20-21 data
-st.dataframe(df)
+st.text("""And everyone clapped.""")
 
 
-
-table = pd.pivot_table(df, values='student_count', index=['week'],
-                       columns=['learning_modality'], aggfunc="sum")
-
-table = table.reset_index()
-table.columns
-
-## line chart by week 
-st.bar_chart(
-    table,
-    x="week",
-    y="Hybrid",
-)
-
-st.bar_chart(
-    table,
-    x="week",
-    y="In Person",
-)
-
-st.bar_chart(
-    table,
-    x="week",
-    y="Remote",
-)
+# Display a GIF
+gif_path = "C:\\Users\\grift\\OneDrive\\Documents\\GitHub\\streamlit-dashboard-enhanced\\clap gif.webp" 
+st.image(gif_path, caption="Your GIF", use_container_width=True)
